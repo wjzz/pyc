6451f5e1526fed6f1a40e2c0b033c179a8542fcd
wjzz <wjedynak@gmail.com>
2019-10-26 21:29:03 +0900

    Update a flaky e2e test and improve the e2e test runner

--- scripts/test_all.sh
+++ scripts/test_all.sh
@@ -1,12 +1,21 @@
+#!/bin/bash
+
 # compiles all examples and compares the contents from corresponding .output files
+
+exit_code=0  # all OK
 
 for file in examples/*.sil
 do 
     echo $file
     filename=`basename "$file"`
     base=`basename "$file" .sil`    # part without the extension
-    (./scripts/compile.sh $file > /dev/null && ./target/$base >tmpfile && diff tmpfile examples/outputs/$base.output) \
-      || echo "ERROR: $file"
+    ./scripts/compile.sh $file > /dev/null && ./target/$base >tmpfile && diff tmpfile examples/outputs/$base.output
+    if [ $? -ne 0 ]
+    then
+      exit_code=1
+      echo "ERROR: $file"
+    fi
 done
 rm -f tmpfile
 
+exit $exit_code
