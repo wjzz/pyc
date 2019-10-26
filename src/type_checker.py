ae143cdc2fb51bd7ca5a6eb036bde506142c0121
wjzz <wjedynak@gmail.com>
2019-10-27 02:12:00 +0900

    Implement v1 of scope checking

--- src/type_checker.py
+++ src/type_checker.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 """
 Type checking of C programs.
 
@@ -6,11 +8,10 @@ We check that:
 - there are no type errors
 """
 
-from typing import List, Any
+from typing import Any, Optional, List, Dict
 
-Stm = Any
-SymbolTable = Any
-
+from visitor import Visitor
+from ast import FunDecl, LValue, FunArg
 
 class CTypeError(Exception):
     def __init__(self, msg):
@@ -19,12 +20,156 @@ class CTypeError(Exception):
     def __str__(self):
         return f"{self.msg}"
 
+Stm = Any
+Var = str
+Tp = str
+Params = List[FunArg]
 
-def check(c: List[Stm]) -> SymbolTable:
+
+class Env:
+    env : Dict[Var, Tp] = {}
+
+    def __contains__(self, var: Var):
+        return var in self.env
+
+    def add(self, var: Var, tp: Tp):
+        self.env[var] = tp
+
+    def copy(self) -> Env:
+        cp = Env()
+        cp.env = self.env.copy()
+        return cp
+
+    def __str__(self):
+        inside = ",".join([f"{k}:{v}" for k,v in self.env.items()])
+        return f"[{inside}]"
+
+class SymbolTable:
+    pass
+
+
+class TypeCheckingVisitor(Visitor):
+    symbol_table : SymbolTable = SymbolTable()
+    env : Env = Env()
+
+    # environment
+
+    def add_binding(self, var: Var, tp: Tp):
+        self.env.add(var, tp)
+
+    def expect_bound_var(self, var: Var):
+        if var not in self.env:
+            raise CTypeError(f"name {var} is not defined in env: {self.env}")
+
+    # type checking
+
+    def __init__(self):
+        pass
+
+    def visit_ArithLit(self, val):
+        pass
+
+    def visit_Var(self, var: Var):
+        self.expect_bound_var(var)
+
+    def visit_FunCall(self, name: Var, args: List[Any]):
+        # TODO: check if C has a different namespace for functions
+        self.expect_bound_var(name)
+        self.visit_many(args)
+
+    def visit_ArithUnaryop(self, op, a):
+        a.accept(self)
+
+    def visit_ArithBinop(self, op, a1, a2):
+        a1.accept(self)
+        a2.accept(self)
+
+    def visit_BoolArithCmp(self, op, a1, a2):
+        a1.accept(self)
+        a2.accept(self)
+
+    def visit_BoolNeg(self, b):
+        b.accept(self)
+
+    def visit_BoolBinop(self, op, b1, b2):
+        b1.accept(self)
+        b2.accept(self)
+
+    def visit_ArithAssign(self, lvalue: LValue, a):
+        var: Var = lvalue.loc
+        self.expect_bound_var(var)
+        a.accept(self)
+
+    def visit_StmExpr(self, a):
+        a.accept(self)
+
+    def visit_StmDecl(self, tp: Tp, var: Var, a: Optional[Any], kind):
+        if a is not None:
+            a.accept(self)
+        self.add_binding(var, tp)
+
+    def visit_StmIf(self, b, ss1, ss2):
+        b.accept(self)
+        self.visit_StmBlock(ss1)
+        self.visit_StmBlock(ss2)
+
+    def visit_StmWhile(self, b, ss):
+        b.accept(self)
+        self.visit_StmBlock(ss)
+
+    def visit_StmPrint(self, a):
+        a.accept(self)
+
+    def visit_StmReturn(self, a):
+        a.accept(self)
+
+    def visit_StmBreak(self):
+        pass
+
+    def visit_StmContinue(self):
+        pass
+
+    def visit_StmBlock(self, stms):
+        env = self.env.copy()
+        self.visit_many(stms)
+        self.env = env
+
+    def visit_many(self, stms):
+        for stm in stms:
+            stm.accept(self)
+
+    def visit_FunDecl(self, tp: Tp, name: Var, params: Params, body: List[Stm]):
+        # TODO: extend the env to handle recursive calls
+
+        self.add_binding(name, tp)
+
+        env = self.env.copy()
+
+        for funarg in params:
+            self.add_binding(funarg.var, funarg.type)
+
+        self.visit_many(body)
+
+        self.env = env
+
+    def visit_many_defs(self, defs):
+        fun_names = [defn.name for defn in defs
+             if isinstance(defn, FunDecl) ]
+        if "main" not in fun_names:
+            msg = ("A program with function definition must have" +
+                " a 'main' function! Aborting...")
+            raise CTypeError(msg)
+
+        return self.visit_many(defs)
+
+
+def check(defs: List[Stm]) -> SymbolTable:
     """
-    Type checks the given program.
+    Type checks the given program and returns a symbol table
+    containing the type information for all nodes in the program.
 
     Raises a CTypeError if there are some problems with the given input.
     """
-    symbol_table = None
-    return symbol_table
+    visitor = TypeCheckingVisitor()
+    visitor.visit_many_defs(defs)
+    return visitor.symbol_table
