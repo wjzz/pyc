4e38f8a2a587de9b6811cc5b7c2ac9c1ffcf9848
wjzz <wjedynak@gmail.com>
2019-10-27 01:08:40 +0900

    Return exit code 2 for incorrect inputs

--- scripts/test_errors.sh
+++ scripts/test_errors.sh
@@ -9,11 +9,12 @@ do
     echo $file
     filename=`basename "$file"`
     base=`basename "$file" .sil`    # part without the extension
-    python src/main.py $file > /dev/null 2> /dev/null
-    if [ $? -ne 1 ]
+    python src/main.py $file >tmpfile 2>tmpfile
+    if [ $? -ne 2 ]
     then
       exit_code=1
       echo "NO ERROR FOUND FOR INCORRENT FILE: $file"
+      cat tmpfile
     fi
 done
 rm -f tmpfile
